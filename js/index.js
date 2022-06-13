9a1a02c0963dfedf5dec45f864857496d7350d75
Beksultan1602 <90913576+Beksultan1602@users.noreply.github.com>
2022-06-13 15:14:31 +0600

    knoweledge add

--- js/index.js
+++ js/index.js
@@ -12,3 +12,15 @@ hamb.addEventListener('click', () => {
 		hamb.classList.add('active')
 	}
 })
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (event) {
+		event.preventDefault()
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+}
